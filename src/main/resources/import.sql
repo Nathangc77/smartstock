0941628fd2ce330b128851af03469397a57a70cd
Nathan Moreira <nathangc7@gmail.com>
2025-03-17 19:12:46 -0300

    "feat: User-Role model and seeding database"

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -36,3 +36,13 @@ INSERT INTO tb_product_provider (product_id, provider_id) VALUES (10, 4);
 INSERT INTO tb_product_provider (product_id, provider_id) VALUES (11, 1);
 INSERT INTO tb_product_provider (product_id, provider_id) VALUES (12, 4);
 INSERT INTO tb_product_provider (product_id, provider_id) VALUES (13, 4);
+
+INSERT INTO tb_user (name, email, password) VALUES ('Maria Brown', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex Green', 'alex@gmail.com', '123456');
+
+INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 2);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
