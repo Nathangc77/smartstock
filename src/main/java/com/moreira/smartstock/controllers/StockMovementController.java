5ce93c5eaadf29c6429c20ad0bef8b342d536ace
Nathan Moreira <nathangc7@gmail.com>
2025-03-28 10:17:52 -0300

    "refactor: restructure old stock movement system"

--- src/main/java/com/moreira/smartstock/controllers/StockMovementController.java
+++ src/main/java/com/moreira/smartstock/controllers/StockMovementController.java
@@ -2,13 +2,13 @@ package com.moreira.smartstock.controllers;
 
 import com.moreira.smartstock.dtos.StockMovementDTO;
 import com.moreira.smartstock.services.StockMovementService;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/movements")
@@ -23,10 +23,4 @@ public class StockMovementController {
         return ResponseEntity.ok(result);
     }
 
-    @PostMapping
-    @PreAuthorize("hasAnyRole('ROLE_OPERATOR', 'ROLE_ADMIN')")
-    public ResponseEntity<StockMovementDTO> movement(@RequestBody @Valid StockMovementDTO dto) {
-        dto = service.movement(dto);
-        return ResponseEntity.ok(dto);
-    }
 }
--- src/main/java/com/moreira/smartstock/services/StockMovementService.java
+++ src/main/java/com/moreira/smartstock/services/StockMovementService.java
@@ -1,20 +1,14 @@
 package com.moreira.smartstock.services;
 
 import com.moreira.smartstock.dtos.StockMovementDTO;
-import com.moreira.smartstock.entities.Product;
 import com.moreira.smartstock.entities.StockMovement;
-import com.moreira.smartstock.entities.TypeMovement;
 import com.moreira.smartstock.repositories.ProductRepository;
 import com.moreira.smartstock.repositories.StockMovementRepository;
-import com.moreira.smartstock.repositories.UserRepository;
-import com.moreira.smartstock.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.Instant;
 
 @Service
 public class StockMovementService {
@@ -34,31 +28,4 @@ public class StockMovementService {
         return result.map(StockMovementDTO::new);
     }
 
-    @Transactional
-    public StockMovementDTO movement(StockMovementDTO dto) {
-        Product product = productRepository.findById(dto.getProductId()).orElseThrow(
-                () -> new ResourceNotFoundException("Produto não encontrado")
-        );
-
-        if (dto.getType().equals(TypeMovement.ENTRADA.name())) {
-            product.setQuantity(product.getQuantity() + dto.getQuantity());
-        } else if (dto.getType().equals(TypeMovement.SAIDA.name())) {
-            if (dto.getQuantity() > product.getQuantity()) throw new RuntimeException("Estoque insuficiente");
-            product.setQuantity(product.getQuantity() - dto.getQuantity());
-        } else {
-            throw new ResourceNotFoundException("Recurso não encontrado");
-        }
-
-        productRepository.save(product);
-
-        StockMovement entity = new StockMovement();
-        entity.setQuantity(dto.getQuantity());
-        entity.setMoment(Instant.now());
-        entity.setType(TypeMovement.valueOf(dto.getType()));
-        entity.setReason(dto.getReason());
-        entity.setProduct(product);
-        entity.setUser(userService.getUserLogged());
-        entity = repository.save(entity);
-        return new StockMovementDTO(entity);
-    }
 }
