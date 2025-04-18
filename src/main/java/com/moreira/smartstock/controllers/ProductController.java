23e0a2ef50179acd3c2f1bf95a32bf2c2cbb2dda
Nathan Moreira <nathangc7@gmail.com>
2025-04-18 15:36:03 -0300

    "docs: add Swagger annotations and descriptions to controllers"

--- src/main/java/com/moreira/smartstock/controllers/ProductController.java
+++ src/main/java/com/moreira/smartstock/controllers/ProductController.java
@@ -3,6 +3,10 @@ package com.moreira.smartstock.controllers;
 import com.moreira.smartstock.dtos.ProductSaveDTO;
 import com.moreira.smartstock.dtos.ProductDTO;
 import com.moreira.smartstock.services.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -15,12 +19,18 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 
 @RestController
-@RequestMapping(value = "/products")
+@RequestMapping(value = "/products", produces = "application/json")
+@SecurityRequirement(name = "bearerAuth")
 public class ProductController {
 
     @Autowired
     private ProductService service;
 
+    @Operation(summary = "Busca todos os usuários")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized")
+    })
     @GetMapping
     @PreAuthorize("hasAnyRole('ROLE_OPERATOR', 'ROLE_ADMIN')")
     public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
@@ -28,6 +38,13 @@ public class ProductController {
         return ResponseEntity.ok(results);
     }
 
+    @Operation(summary = "Busca usuário por Id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "403", description = "Forbidden"),
+            @ApiResponse(responseCode = "404", description = "Not Found")
+    })
     @GetMapping(value = "/{id}")
     @PreAuthorize("hasAnyRole('ROLE_OPERATOR', 'ROLE_ADMIN')")
     public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
@@ -35,6 +52,14 @@ public class ProductController {
         return ResponseEntity.ok(dto);
     }
 
+    @Operation(summary = "Cria um novo produto")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Created"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "403", description = "Forbidden"),
+            @ApiResponse(responseCode = "404", description = "Not Found"),
+            @ApiResponse(responseCode = "422", description = "Unprocessable Entity")
+    })
     @PostMapping
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductSaveDTO dto) {
@@ -46,6 +71,15 @@ public class ProductController {
         return ResponseEntity.created(uri).body(inserted);
     }
 
+    @Operation(summary = "Atualiza um produto")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "400", description = "Bad Request"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "403", description = "Forbidden"),
+            @ApiResponse(responseCode = "404", description = "Not Found"),
+            @ApiResponse(responseCode = "422", description = "Unprocessable Entity")
+    })
     @PutMapping(value = "/{id}")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<ProductDTO> update(@PathVariable Long id, @Valid @RequestBody ProductSaveDTO dto) {
@@ -53,6 +87,14 @@ public class ProductController {
         return ResponseEntity.ok().body(updated);
     }
 
+    @Operation(summary = "Deleta um produto")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "No Content"),
+            @ApiResponse(responseCode = "400", description = "Bad Request"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "403", description = "Forbidden"),
+            @ApiResponse(responseCode = "404", description = "Not Found")
+    })
     @DeleteMapping(value = "/{id}")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
--- src/main/java/com/moreira/smartstock/controllers/StockMovementController.java
+++ src/main/java/com/moreira/smartstock/controllers/StockMovementController.java
@@ -4,6 +4,10 @@ import com.moreira.smartstock.dtos.EntryMovementDTO;
 import com.moreira.smartstock.dtos.ExitMovementDTO;
 import com.moreira.smartstock.dtos.StockMovementDTO;
 import com.moreira.smartstock.services.StockMovementService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -16,12 +20,18 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 
 @RestController
-@RequestMapping(value = "/movements")
+@RequestMapping(value = "/movements", produces = "application/json")
+@SecurityRequirement(name = "bearerAuth")
 public class StockMovementController {
 
     @Autowired
     private StockMovementService service;
 
+    @Operation(summary = "Busca todas as movimentações")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+    })
     @GetMapping
     @PreAuthorize("hasAnyRole('ROLE_OPERATOR', 'ROLE_ADMIN')")
     public ResponseEntity<Page<StockMovementDTO>> findAll(Pageable pageable) {
@@ -29,6 +39,12 @@ public class StockMovementController {
         return ResponseEntity.ok(result);
     }
 
+    @Operation(summary = "Cria uma movimentação de entrada")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Created"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "404", description = "Not Found")
+    })
     @PostMapping(value = "/entry")
     @PreAuthorize("hasAnyRole('ROLE_OPERATOR', 'ROLE_ADMIN')")
     public ResponseEntity<StockMovementDTO> registerStockEntry(@Valid @RequestBody EntryMovementDTO dto) {
@@ -40,6 +56,12 @@ public class StockMovementController {
         return ResponseEntity.created(uri).body(result);
     }
 
+    @Operation(summary = "Cria uma movimentação de saída")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Created"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "404", description = "Not Found")
+    })
     @PostMapping(value = "/exit")
     @PreAuthorize("hasAnyRole('ROLE_OPERATOR', 'ROLE_ADMIN')")
     public ResponseEntity<StockMovementDTO> registerStockExit(@Valid @RequestBody ExitMovementDTO dto) {
